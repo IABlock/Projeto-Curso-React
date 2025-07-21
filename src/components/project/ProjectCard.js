c157f61a37190d0428347232417101465a216aa0
Rene Paulo Taba da Silva <renetaba@gmail.com>
2025-07-21 17:10:33 -0300

    Style do Card e Botoes

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,18 +1,27 @@
+import React from 'react';
+import {BsPencil, BsFillTrashFill} from 'react-icons/bs';
+import {Link} from 'react-router-dom';
+
 import styles from './ProjectCard.module.css';
 
+
+
 function ProjectCard({ id, name, budget, category, handleRemove }) {
+    
+    const categoryLowerCase = category.toLowerCase();
+    
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
             <p>
                 <span>Orçamento:</span> R$ {budget}
             </p>
-            <p>
-                <span>Categoria:</span> {category.name}
+            <p className={styles.category_text}>                
+                <span className={`${styles[categoryLowerCase]}`}></span> {category}
             </p>
             <div className={styles.project_card_actions}>
-                <p>Excluir</p>
-                <p>Editar</p>
+                <Link to='/'><BsPencil />Editar</Link>
+                <button><BsFillTrashFill />Excluir</button>
             </div>
         </div>
     );
