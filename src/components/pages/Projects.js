91620fcfb6e3f764e85144e4256c098e1894d4cf
Rene Paulo Taba da Silva <renetaba@gmail.com>
2025-07-21 11:54:22 -0300

    Componentes de menssagem

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,22 @@
+import { useLocation } from 'react-router-dom';
+
+import Message from '../layout/Message';
+
 function Projects() {
+    const location = useLocation();
+    console.log('Location:', location);
+    console.log('Location state:', location.state);
+    let message = '';
+    if (location.state) {
+        message = location.state.message;
+        console.log('Message from location state:', message);
+    }
+
   return (
     <div className="projects">
       <h1>Welcome to the Projects Page</h1>
       <p>This is the main content of the projects page.</p>
+      <Message type="success" msg={message} />
     </div>
   );
 }
